2e9e3836dba809a813623b86b7272734832003ad
derekdkim <ddkim1024@gmail.com>
2020-03-20 14:57:46 -0500

    Added erase and resize features.

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Vanilla JS Sketchpad
 
-## To-do:
-- Fill while mouse is dragging across the sketchpad.
-- Resize feature for grid (refactor CSS grid settings).
-- Erase feature.
-- Pretty up the page itself and center elements.
+Made while going through Odin Project.
--- index.js
+++ index.js
@@ -1,47 +1,67 @@
-// Make 16 x 16 grid of divs
-const setGrid = () => {
+// Make a grid of divs up to 960
+const setGrid = (maxX, maxY) => {
   const container = document.querySelector('#grid-container');
-  for (let x = 0; x < 16; x++) {
-    for (let y = 0; y < 16; y++) {
+  container.style.setProperty('--rowNum', maxY);
+  container.style.setProperty('--colNum', maxX);
+
+  for (let x = 0; x < maxX; x++) {
+    for (let y = 0; y < maxY; y++) {
       const block = document.createElement('div');
       block.classList.add('grid');
-      block.setAttribute('id', `${(x + 1) * (y + 1)}`);
+      block.setAttribute('id', `${(x * maxX) + (y + 1)}`);
       container.appendChild(block);
     }
   }
 };
 
+// Checks if input is valid
+const isInputValid = (input) => {
+  if (typeof input === 'number' && input > 0 && input <= 200) {
+    return true;
+  } else {
+    alert('That number is invalid. Please enter a number from 1 to 200.');
+    return false;
+  }
+};
+
 // Main function on load
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("This thing started.");
-  setGrid();
+  setGrid(16, 16);
+  updateState();
+});
 
+// Reset function
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('click', () => {
+  const size = document.getElementById('size-input');
+  // Remove previous grid
+  document.querySelectorAll('.grid').forEach(el => el.remove());
+  // Create new grid
+  const inputNum = Number(size.value);
+  if (isInputValid(inputNum)) {
+    setGrid(inputNum, inputNum);
+  } else {
+    setGrid(16, 16);
+  }
+  updateState();
+});
+
+// Erase function
+const eraseButton = document.getElementById('erase');
+
+eraseButton.addEventListener('click', () => {
+  document.querySelectorAll('.filled').forEach(el => el.classList.remove('filled'));
+});
+
+// Allow for erase and fill functions to work properly
+const gridArr = document.getElementsByClassName('grid');
+
+const updateState = () => {
+  // Fill in colour when clicked.
   for (let i = 0; i < gridArr.length; i++) {
-    console.log(gridArr[i]);
-    gridArr[i].addEventListener('mousedown', () => {
-      console.log("Click event triggered.");
+    gridArr[i].addEventListener('mouseover', () => {
       gridArr[i].classList.add('filled');
     });
   }
-});
-
-// On-click colour event
-let gridArr = document.getElementsByClassName('grid');
-console.log(gridArr);
-let gridArr2 = [...document.querySelectorAll('.grid')];
-console.log(gridArr2);
-
-// gridArr2.forEach(element => element.addEventListener('click', () => {
-//   console.log("Click event triggered.");
-//   element.ClassList.add('filled');
-// }));
-
-for (let i = 0; i < gridArr.length; i++) {
-  console.log(gridArr[i]);
-  gridArr[i].addEventListener('click', () => {
-    console.log("Click event triggered.");
-    gridArr[i].style.backgroundColor = 'black';
-  });
-}
-
-
+};
